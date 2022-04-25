9f3da62e66fb1dae25ecb634f62ad853036a9067
Anton Petrov <anton.a.petrov@gmail.com>
2022-04-25 20:52:49 +0300

    Images

--- app/client.py
+++ app/client.py
@@ -1,12 +1,15 @@
 import json
 from rich import print
 import sseclient
-
+import requests
 
 def get_response(url, headers):
     import urllib3
     http = urllib3.PoolManager()
     return http.request('GET', url, preload_content=False, headers=headers)
+
+def _get_response(url, headers):
+    return requests.get(url, stream=True, headers=headers)
 
 
 url = 'http://localhost:8000/stream-data'
